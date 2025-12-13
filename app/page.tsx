55a3a35c36a087cec40334ba4b722e7ee8e93972
Shawn Price <hi@shawnprice.com>
2025-12-13 06:46:48 -0800

    Add homepage button to app

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
     <div className="flex min-h-dvh items-center justify-center">
-      <h1 className="font-medium text-2xl">
-        🇨🇦 Federal Parliament <span className="amp">&</span> Legislation
-        Workbench
-      </h1>
+      <div className="flex flex-col items-center gap-10">
+        <h1 className="font-medium text-2xl">
+          🇨🇦 Federal Parliament <span className="amp">&</span> Legislation
+          Workbench
+        </h1>
+        <Link
+          className="rounded-md bg-primary px-4 py-2 text-primary-foreground hover:bg-primary/90"
+          href="/workbench"
+        >
+          Use Workbench
+        </Link>
+      </div>
     </div>
   );
 }
